9010d124fe9808396e089ffbf143065b6bf8947a
Your Name <you@example.com>
2026-06-01 13:09:32 +0100

    bugfix(rate-limit): disable global rate-limit headers to resolve ASGI Response signature crash

--- api/ratelimit.py
+++ api/ratelimit.py
@@ -2,7 +2,7 @@ from slowapi import Limiter
 from slowapi.util import get_remote_address
 
 
-limiter = Limiter(key_func=get_remote_address, default_limits=["60/minute"], headers_enabled=True)
+limiter = Limiter(key_func=get_remote_address, default_limits=["60/minute"], headers_enabled=False)
 
 
 if __name__ == "__main__":
